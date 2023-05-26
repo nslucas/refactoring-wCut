61c60b6b2c39f1f3b50577cb250cfb608b3d6888
Lucas Nunes <lucasnunes.santos088@gmail.com>
2023-05-25 23:37:51 -0300

    projeto criado, classes Modulo e CalculadoraTamanhoPecas criadas

---
+++ App/Entidades/CalculadoraTamanhoPecas.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wCut.App
+{
+    public class CalculadoraTamanhoPecas
+    {
+       
+        public static String CalcularTamanhoPortas(int quantidade, double altura, double largura, int espessura)
+        {
+            int puxador = 35;
+            double alturaPorta = altura - 10 - puxador;
+            double larguraPorta = largura / 2 - 10;
+            string resultado = quantidade + "x" + alturaPorta + "*" + larguraPorta + espessura;
+            return resultado;
+        }
+
+        public static String CalcularTamanhoLateral(int quantidade, double altura, double profundidade, int espessura)
+        {
+            string resultado = quantidade + "x" + altura + "*" + profundidade + "*" + espessura;
+            return resultado;
+            
+        }
+    }
+}
